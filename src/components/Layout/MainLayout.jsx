b6853a024c5962ef99ae064066d5f2076c91eaf4
Kelly Kim <hi@kellykim.co>
2023-03-04 16:28:17 -0700

    initial ui commit with tailwind config, unauthenticated homepage and unauthenticated navbar

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,10 +1,11 @@
 import { Unstable_Grid2 } from "@mui/material";
 import Navbar from "../Navigation/Navbar";
+
 export default function MainLayout({ children }) {
   return (
-    <Unstable_Grid2 sx={{ padding: "2rem 4rem 0.8rem 3rem", margin: "2em" }}>
+    <div className="justify-between p-7 m-7">
       <Navbar />
       {children}
-    </Unstable_Grid2>
+    </div>
   );
 }
